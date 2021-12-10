6a995988f75e04e3a1048ad7b0635dd1397fd38e
Javier Mauricio <javierma36@gmail.com>
2021-12-10 14:26:26 -0500

    Add docs

---
+++ docs/precache-manifest.b64862c826361a1b24f015934eba08c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118bb12da3be7cb1eee1d971aba89044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7824928acd116ae29626",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7824928acd116ae29626",
+    "url": "/static/js/main.4f97ecc3.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
